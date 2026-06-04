02d5f0f4672e41866d6f5a4067bd2e740fde5370
Akhand Soni <akhandsoni2007@gmail.com>
2026-06-04 13:13:13 +0530

    initial database

---
+++ database/migrations/001_create_activity.sql
@@ -0,0 +1,9 @@
+CREATE TABLE activity (
+    activity_id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    repo_id UUID NOT NULL,
+    time_spent TIMESTAMP,
+    likelihood_count INT DEFAULT 0,
+    swipes VARCHAR(10),
+    is_saved BOOL DEFAULT FALSE
+);

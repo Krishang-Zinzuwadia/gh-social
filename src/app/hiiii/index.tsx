fda6e4f78abcc239fea7491108c2eecd7e59e14a
Tarun <tarunram23@gmail.com>
2026-06-17 16:39:23 +0530

    Delete src/app/hiiii/index.tsx

--- src/app/hiiii/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Hello</Text>
-    </View>
-  );
-}
